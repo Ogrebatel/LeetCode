e55f361e3b63d8575b49c4199f5c7178a0998e4d
ogrebatel <ogrebatelb@gmail.com>
2023-08-18 18:24:24 +0300

    complete

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,27 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+int Solution::removeDuplicates(std::vector<int> &nums) {
+    int curIdx = 0;
+    int count = 0;
+    int i = 0;
+    while(i < nums.size()) {
+        if (nums[curIdx] == nums[i]) {
+
+            while(i < nums.size() && nums[curIdx] == nums[i]) {
+                i++;
+            }
+
+            if(i >= nums.size()) {
+                count++;
+                return count;
+            }
+            curIdx++;
+            nums[curIdx] = nums[i];
+            count++;
+
+        } else {
+            i++;
+        }
+    }
+    return count;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,10 +1,37 @@
 #include <gtest/gtest.h>
 #include "Solution.h"
 
-TEST(Test, test_for_test) {
+TEST(Test, Example1) {
     Solution sol;
+    std::vector<int> nums = {1,1,2};
+    EXPECT_EQ(2, sol.removeDuplicates(nums));
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+    for (auto &num: nums) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+TEST(Test, Example0) {
+    Solution sol;
+    std::vector<int> nums = {1,2};
+    EXPECT_EQ(2, sol.removeDuplicates(nums));
+
+    for (auto &num: nums) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+TEST(Test, Example2) {
+    Solution sol;
+    std::vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    EXPECT_EQ(5, sol.removeDuplicates(nums));
+
+    for (auto &num: nums) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main() {
