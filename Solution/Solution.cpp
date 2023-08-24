d8e7d8661f1da9d0786a487b8104e1d399248b0d
ogrebatel <ogrebatelb@gmail.com>
2023-08-24 17:23:53 +0300

    complete

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,16 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+std::vector<int> Solution::twoSum(std::vector<int> &numbers, int target) {
+    int i = 0;
+    int j = numbers.size() - 1;
+
+    while(true) {
+        if (numbers[i] + numbers[j] < target) {
+            ++i;
+        } else if (numbers[i] + numbers[j] > target) {
+            --j;
+        } else {
+            return {++i, ++j};
+        }
+    }
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,10 +1,28 @@
 #include <gtest/gtest.h>
 #include "Solution.h"
 
-TEST(Test, test_for_test) {
+TEST(Test, Example1) {
     Solution sol;
+    std::vector<int> numbers = {2,7,11,15};
+    std::vector<int> expected = {1, 2};
+    EXPECT_EQ(expected[0], sol.twoSum(numbers, 9)[0]);
+    EXPECT_EQ(expected[1], sol.twoSum(numbers, 9)[1]);
+}
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+TEST(Test, Example2) {
+    Solution sol;
+    std::vector<int> numbers = {2,3,4};
+    std::vector<int> expected = {1, 3};
+    EXPECT_EQ(expected[0], sol.twoSum(numbers, 6)[0]);
+    EXPECT_EQ(expected[1], sol.twoSum(numbers, 6)[1]);
+}
+
+TEST(Test, Example3) {
+    Solution sol;
+    std::vector<int> numbers = {-1, 0};
+    std::vector<int> expected = {1, 2};
+    EXPECT_EQ(expected[0], sol.twoSum(numbers, -1)[0]);
+    EXPECT_EQ(expected[1], sol.twoSum(numbers, -1)[1]);
 }
 
 int main() {
