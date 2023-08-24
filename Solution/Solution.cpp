a9f88cfca3fd3603c9d756a5f5f05cda83587779
ogrebatel <ogrebatelb@gmail.com>
2023-08-24 15:47:34 +0300

    complete

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,36 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+bool Solution::canJump(std::vector<int> &nums) {
+    if (nums.empty()) {
+        return false;
+    }
+    int numsSize = nums.size();
+    if(numsSize != 1 && nums[0] == 0) {
+        return false;
+    }
+
+    int curJump = nums[0];
+    int maxJump = 0;
+
+    for (int i = 1; i < numsSize; ++i) {
+        curJump--;
+        if(nums[i] - curJump > maxJump) {
+//            if (nums[i] + i >= numsSize - 1) {
+//                return true;
+//            }
+            maxJump = nums[i] - curJump;
+        }
+
+        if(curJump == 0) {
+            if(i == numsSize - 1) {
+                return true;
+            }
+            if(maxJump == 0) {
+                return false;
+            }
+            curJump = maxJump;
+            maxJump = 0;
+        }
+    }
+    return true;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,10 +1,22 @@
 #include <gtest/gtest.h>
 #include "Solution.h"
 
-TEST(Test, test_for_test) {
+TEST(Test, Example1) {
     Solution sol;
+    std::vector<int> nums = {2,3,1,1,4};
+    EXPECT_TRUE(sol.canJump(nums));
+}
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+TEST(Test, Example2) {
+    Solution sol;
+    std::vector<int> nums = {3,2,1,0,4};
+    EXPECT_FALSE(sol.canJump(nums));
+}
+
+TEST(Test, Example3) {
+    Solution sol;
+    std::vector<int> nums = {1,1,0,1};
+    EXPECT_FALSE(sol.canJump(nums));
 }
 
 int main() {
