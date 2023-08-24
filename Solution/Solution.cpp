25dfd3c9fe29ebe19b29c176a1338d2ed82fab6e
ogrebatel <ogrebatelb@gmail.com>
2023-08-24 16:00:46 +0300

    complete

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,43 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+int Solution::jump(std::vector<int> &nums) {
+    if (nums.empty()) {
+        return false;
+    }
+
+    int numsSize = nums.size();
+    if(numsSize == 1) {
+        return 0;
+    }
+    if(nums[0] >= numsSize - 1) {
+        return 1;
+    }
+
+    int jumpCount = 1;
+
+    int curJump = nums[0];
+    int maxJump = 0;
+
+    for (int i = 1; i < numsSize; ++i) {
+        curJump--;
+        if(nums[i] - curJump > maxJump) {
+            if (nums[i] + i >= numsSize - 1) {
+                return jumpCount + 1;
+            }
+            maxJump = nums[i] - curJump;
+        }
+
+        if(curJump == 0) {
+            jumpCount++;
+            if(i == numsSize - 1) {
+                return jumpCount;
+            }
+            if(maxJump == 0) {
+                return -1;
+            }
+            curJump = maxJump;
+            maxJump = 0;
+        }
+    }
+    return jumpCount;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,11 +1,17 @@
 #include <gtest/gtest.h>
 #include "Solution.h"
 
-TEST(Test, test_for_test) {
+TEST(Test, Example1) {
     Solution sol;
-
-    EXPECT_EQ(4, sol.solution(2, 2));
+    std::vector<int> nums = {2,3,1,1,4};
+    EXPECT_EQ(2, sol.jump(nums));
 }
+TEST(Test, Example2) {
+    Solution sol;
+    std::vector<int> nums = {2,3,0,1,4};
+    EXPECT_EQ(2, sol.jump(nums));
+}
+
 
 int main() {
     ::testing::InitGoogleTest();
