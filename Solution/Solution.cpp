3b1c21593cc987be8b0640089d711254389376a2
ogrebatel <ogrebatelb@gmail.com>
2023-08-28 13:41:01 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,22 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+int Solution::canCompleteCircuit(std::vector<int> &gas, std::vector<int> &cost) {
+    for (int i = 0; i < gas.size(); ++i) {
+        if (gas[i] >= cost[i]) {
+            int tank = 0;
+            int j = i;
+            for (; j < gas.size() + i; ++j) {
+                tank = tank + gas[j % gas.size()] - cost[j % gas.size()];
+                if (tank < 0) {
+                    break;
+                }
+            }
+            if (tank < 0) {
+                i = j;
+                continue;
+            }
+            return i;
+        }
+    }
+    return -1;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,10 +1,20 @@
 #include <gtest/gtest.h>
 #include "Solution.h"
 
-TEST(Test, test_for_test) {
+TEST(Test, Example_1) {
     Solution sol;
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+    std::vector<int> gas = {1,2,3,4,5};
+    std::vector<int> cost = {3,4,5,1,2};
+    EXPECT_EQ(3, sol.canCompleteCircuit(gas, cost));
+}
+
+TEST(Test, Example_2) {
+    Solution sol;
+
+    std::vector<int> gas = {2,3,4};
+    std::vector<int> cost = {3,4,3};
+    EXPECT_EQ(-1, sol.canCompleteCircuit(gas, cost));
 }
 
 int main() {
