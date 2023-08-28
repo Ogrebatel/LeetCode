c7f4ccd85ce28cebc57bd777cd9adcda466795b4
ogrebatel <ogrebatelb@gmail.com>
2023-08-28 18:27:25 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,48 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+int Solution::candy(std::vector<int> &ratings) {
+    if (ratings.size() == 1) {
+        return 1;
+    }
+    int totalCandies = 0;
+    int currentCost = 1;
+    for (int i = 0; i < ratings.size() - 1; ++i) {
+        if (ratings[i] > ratings[i + 1]) {
+            int j = i;
+            int rightCost = 1;
+            while (j < ratings.size() - 1 && ratings[j] > ratings[j + 1]) {
+                ++rightCost;
+                ++j;
+            }
+
+            if (rightCost > currentCost) {
+                totalCandies += rightCost;
+            } else {
+                totalCandies += currentCost;
+            }
+            --rightCost;
+            for (; rightCost > 0; --rightCost) {
+                totalCandies += rightCost;
+            }
+
+            i = j;
+            currentCost = 2;
+
+            if (j < ratings.size() - 1 && ratings[i] == ratings[i + 1]) {
+                currentCost = 1;
+            }
+        } else if (ratings[i] < ratings[i + 1]){
+            totalCandies += currentCost;
+            ++currentCost;
+        } else {
+            totalCandies += currentCost;
+            currentCost = 1;
+        }
+    }
+    if (ratings.size() > 1 && ratings[ratings.size() - 2] < ratings[ratings.size() - 1]) {
+        totalCandies += currentCost;
+    } else if (ratings.size() > 1 && ratings[ratings.size() - 2] == ratings[ratings.size() - 1]) {
+        ++totalCandies;
+    }
+    return totalCandies;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,11 +1,47 @@
 #include <gtest/gtest.h>
 #include "Solution.h"
 
-TEST(Test, test_for_test) {
+TEST(Test, Example7) {
     Solution sol;
-
-    EXPECT_EQ(4, sol.solution(2, 2));
+    std::vector<int> ratings = {1,2,3,5,4,3,2,1,4,3,2,1,3,2,1,1,2,3,4};
+    EXPECT_EQ(47, sol.candy(ratings));
 }
+
+TEST(Test, Example1) {
+    Solution sol;
+    std::vector<int> ratings = {1,0,2};
+    EXPECT_EQ(5, sol.candy(ratings));
+}
+
+TEST(Test, Example5) {
+    Solution sol;
+    std::vector<int> ratings = {1,2,3};
+    EXPECT_EQ(6, sol.candy(ratings));
+}
+
+TEST(Test, Example4) {
+    Solution sol;
+    std::vector<int> ratings = {3,2,1};
+    EXPECT_EQ(6, sol.candy(ratings));
+}
+
+TEST(Test, Example2) {
+    Solution sol;
+    std::vector<int> ratings = {1,2,2};
+    EXPECT_EQ(4, sol.candy(ratings));
+}
+
+TEST(Test, Example3) {
+    Solution sol;
+    std::vector<int> ratings = {1,3,2,2,1};
+    EXPECT_EQ(7, sol.candy(ratings));
+}
+
+
+
+
+
+
 
 int main() {
     ::testing::InitGoogleTest();
