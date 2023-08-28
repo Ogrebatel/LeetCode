da63d4f2744b87083ef42ca0434a63a5f93d0bae
ogrebatel <ogrebatelb@gmail.com>
2023-08-28 19:42:45 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,22 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+int Solution::strStr(std::string haystack, std::string needle) {
+    if (needle.empty()) {
+        return -1;
+    }
+
+    for (int i = 0; i < haystack.size(); ++i) {
+        if (haystack[i] == needle[0]) {
+            int j = 0;
+            for (; j < needle.size(); ++j) {
+                if (i + j == haystack.size() || haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == needle.size()) {
+                return i;
+            }
+        }
+    }
+    return -1;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,10 +1,25 @@
 #include <gtest/gtest.h>
 #include "Solution.h"
 
-TEST(Test, test_for_test) {
+TEST(Test, Example3) {
     Solution sol;
+    std::string haystack = "mississippi";
+    std::string needle = "issip";
+    EXPECT_EQ(4, sol.strStr(haystack, needle));
+}
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+TEST(Test, Example1) {
+    Solution sol;
+    std::string haystack = "sadbutsad";
+    std::string needle = "sad";
+    EXPECT_EQ(0, sol.strStr(haystack, needle));
+}
+
+TEST(Test, Example2) {
+    Solution sol;
+    std::string haystack = "leetcode";
+    std::string needle = "leeto";
+    EXPECT_EQ(-1, sol.strStr(haystack, needle));
 }
 
 int main() {
