0a8d4475dffb81f3f9321f7c1e3be60e7b854842
ogrebatel <ogrebatelb@gmail.com>
2023-08-29 17:52:19 +0300

    in process

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -2,20 +2,28 @@
 
 
 bool Solution::wordPattern(std::string pattern, std::string s) {
+    std::string result;
+    std::string str;
+    std::stringstream ss(s);
+    std::vector<std::string> v;
+    while (getline(ss, str, ' ')) {
+        if(!str.empty())
+            v.emplace_back(str);
+    }
 
     std::map<char, std::string> equalityS;
     std::map<std::string, char> equalityT;
-    if (s.size() != t.size()) {
+    if (s.size() != v.size()) {
         return false;
     }
     for (int i = 0; i < s.size(); ++i) {
-        if (equalityS.find(s[i]) != equalityS.end() || equalityT.find(t[i]) != equalityT.end()) {
-            if (t[i] != equalityS[s[i]] || s[i] != equalityT[t[i]]) {
+        if (equalityS.find(s[i]) != equalityS.end() || equalityT.find(v[i]) != equalityT.end()) {
+            if (v[i] != equalityS[s[i]] || s[i] != equalityT[v[i]]) {
                 return false;
             }
         } else {
-            equalityS[s[i]] = t[i];
-            equalityT[t[i]] = s[i];
+            equalityS[s[i]] = v[i];
+            equalityT[v[i]] = s[i];
         }
     }
     return true;
--- Solution/Solution.h
+++ Solution/Solution.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <map>
 #include <sstream>
+#include <vector>
 
 class Solution {
  public:
