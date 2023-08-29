289e28a98dda6e9bb5aeb1d7791a8a39f33f7d09
ogrebatel <ogrebatelb@gmail.com>
2023-08-29 18:08:23 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,21 @@
 #include "Solution.h"
 
 bool Solution::isAnagram(std::string s, std::string t) {
-    return false;
+    std::map<char, int> letters;
+    for (const auto &letter: s) {
+        letters[letter]++;
+    }
+
+    for (const auto &letter: t) {
+        letters[letter]--;
+        if (letters[letter] < 0) {
+            return false;
+        }
+    }
+
+    if (std::all_of(letters.begin(), letters.end(), [](std::pair<char, int> iter) {return iter.second == 0;})) {
+        return true;
+    } else {
+        return false;
+    }
 }
