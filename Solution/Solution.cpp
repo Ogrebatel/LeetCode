3403b1c8dabe719b53ca0b3674b85d5e2ebd50ab
Ogrebatel <ogrebatelb@gmail.com>
2023-08-31 14:43:07 +0300

    in process

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,28 @@
 #include "Solution.h"
 
+ListNode *Solution::mergeTwoLists(ListNode *list1, ListNode *list2) {
+    ListNode* result = new ListNode();
+    ListNode* iter = result;
+    while (list1 != nullptr && list2 != nullptr) {
+        if (list1 < list2) {
+            result->val = list1->val;
+            list1 = list1->next;
+        } else {
+            result->val = list2->val;
+            list2 = list2->next;
+        }
+        iter->next = new ListNode();
+    }
+
+    while (list1) {
+        result->val = list1->val;
+        list1 = list1->next;
+    }
+
+    while (list2) {
+        result->val = list2->val;
+        list2 = list2->next;
+    }
+
+    return result;
+}
