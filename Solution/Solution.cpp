33e37536c4487167b3687bec7762f46b6b2f4109
Ogrebatel <ogrebatelb@gmail.com>
2023-08-31 15:45:32 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,27 @@
 #include "Solution.h"
 
+ListNode *Solution::reverseBetween(ListNode *head, int left, int right) {
+    ListNode* iter = head;
+    ListNode* jter;
+
+    --left;
+    --right;
+    int delta = right - left;
+    while (left) {
+        iter = iter->next;
+        --left;
+    }
+
+    while (delta > 0) {
+        jter = iter;
+        int tmpDelta = delta;
+        while (tmpDelta) {
+            jter = jter->next;
+            --tmpDelta;
+        }
+        std::swap(iter->val, jter->val);
+        delta -= 2;
+        iter = iter->next;
+    }
+    return head;
+}
