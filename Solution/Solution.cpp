bc56092d6869e1b89028a7b42b56e579c75cd5d3
Ogrebatel <ogrebatelb@gmail.com>
2023-08-31 16:06:14 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,24 @@
 #include "Solution.h"
 
+ListNode *Solution::removeNthFromEnd(ListNode *head, int n) {
+    ListNode* iter = head;
+    std::vector<ListNode*> addresses;
+
+    while (iter != nullptr) {
+        addresses.push_back(iter);
+        iter = iter->next;
+    }
+
+    if (n == addresses.size()) {
+        iter = head->next;
+        delete head;
+        return iter;
+    }
+
+    int curPos = addresses.size() - n;
+
+    addresses[curPos - 1]->next = addresses[curPos]->next;
+
+    delete iter;
+    return head;
+}
--- Solution/Solution.h
+++ Solution/Solution.h
@@ -1,10 +1,22 @@
 #ifndef LEETCODE_SOLUTION_H
 #define LEETCODE_SOLUTION_H
 
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
- public:
-    static int solution(int a, int b);
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n);
 };
 
 
