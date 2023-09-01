6c1e54a13650680dbc7ba42a7f90f9ea5887bf1f
Ogrebatel <ogrebatelb@gmail.com>
2023-09-01 14:38:06 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,16 @@
 #include "Solution.h"
 
+bool Solution::isSameTree(TreeNode *p, TreeNode *q) {
+    if (p == nullptr && q == nullptr) {
+        return true;
+    }
+
+    if ((p != nullptr && q == nullptr) || (q != nullptr && p == nullptr)) {
+        return false;
+    }
+
+    if (p->val != q->val) {
+        return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
