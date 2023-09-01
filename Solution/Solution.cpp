c9528d33e6a129224ddb1811c79b2e69eaeee7dd
Ogrebatel <ogrebatelb@gmail.com>
2023-09-01 14:50:10 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,11 @@
 #include "Solution.h"
 
+TreeNode *Solution::invertTree(TreeNode *root) {
+    if (!root) {
+        return nullptr;
+    }
+    std::swap(root->left, root->right);
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
--- Solution/Solution.h
+++ Solution/Solution.h
@@ -1,10 +1,23 @@
 #ifndef LEETCODE_SOLUTION_H
 #define LEETCODE_SOLUTION_H
 
+#include <algorithm>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
- public:
-    static int solution(int a, int b);
+public:
+    TreeNode *invertTree(TreeNode *root);
 };
 
 
