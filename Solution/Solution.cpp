0526b2a8ddb5be4b1bb43743a0677a6085eac0c9
Ogrebatel <ogrebatelb@gmail.com>
2023-09-01 14:57:32 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,20 @@
 #include "Solution.h"
 
+bool Solution::isSymmetric(TreeNode *root) {
+    return _isSymmetric(root->left, root->right);
+}
+
+bool Solution::_isSymmetric(TreeNode *a, TreeNode *b) {
+    if (a == nullptr && b == nullptr) {
+        return true;
+    }
+
+    if ((a != nullptr && b == nullptr) || (a == nullptr && b != nullptr)) {
+        return false;
+    }
+
+    if (a->val != b->val) {
+        return false;
+    }
+    return _isSymmetric(a->left, b->right) && _isSymmetric(a->right, b->left);
+}
--- Solution/Solution.h
+++ Solution/Solution.h
@@ -1,10 +1,23 @@
 #ifndef LEETCODE_SOLUTION_H
 #define LEETCODE_SOLUTION_H
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
- public:
-    static int solution(int a, int b);
+public:
+    bool isSymmetric(TreeNode *root);
+private:
+    bool _isSymmetric(TreeNode *a, TreeNode *b);
 };
 
 
