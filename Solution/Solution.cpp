6deee99f5f35d3c119e6eb248b2443e4df0364f5
Ogrebatel <ogrebatelb@gmail.com>
2023-09-02 22:40:34 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,21 @@
 #include "Solution.h"
 
+int Solution::sumNumbers(TreeNode *root) {
+    if (!root) {
+        return 0;
+    } else if (!root->left && !root->right) {
+        return root->val;
+    }
+    return sumNumbers(root->left, root->val) + sumNumbers(root->right, root->val);
+}
+
+int Solution::sumNumbers(TreeNode *root, int val) {
+    if (!root) {
+        return 0;
+    }
+    if (!root->left && !root->right) {
+        return val * 10 + root->val;
+    }
+
+    return sumNumbers(root->left, val * 10 + root->val) + sumNumbers(root->right, val * 10 + root->val);
+}
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -3,8 +3,19 @@
 
 TEST(Test, Example1) {
     Solution sol;
+    TreeNode node1(4);
+    TreeNode node2(9);
+    TreeNode node3(5);
+    TreeNode node4(1);
+    TreeNode node5(0);
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+    node1.left = &node2;
+    node1.right = &node5;
+
+    node2.left = &node3;
+    node2.right = &node4;
+
+    EXPECT_EQ(1026, sol.sumNumbers(&node1));
 }
 
 int main() {
