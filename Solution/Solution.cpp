a02ebca25d7ec3903f0c64776517a79dc6dfdf6f
Ogrebatel <ogrebatelb@gmail.com>
2023-09-04 15:01:19 +0300

    in process

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,42 @@
 #include "Solution.h"
 
+int countHeight(TreeNode* node) {
+    if (!node) {
+        return 0;
+    }
+    if (!node->left && !node->right) {
+        return 1;
+    }
+    return std::max(countHeight(node->left), countHeight(node->right)) + 1;
+}
+
+void levelOrder(TreeNode* node, int i, int position, std::vector<int> *vec) {
+    if (!node) {
+        return;
+    }
+    if (i == 0) {
+        vec->push_back(node->val);
+    } else {
+        levelOrder(node->left, i - 1, position - 1, vec);
+        levelOrder(node->right, i - 1, position + 1, vec);
+    }
+}
+
+
+std::vector<double> Solution::averageOfLevels(TreeNode *root) {
+    if (!root) {
+        return {};
+    }
+    std::vector<double> result;
+    int height = countHeight(root);
+    for (int i = 0; i < height; ++i) {
+        std::vector<int> tmp;
+        levelOrder(root, i, 0, &tmp);
+        double sum = 0;
+        for (const auto &iter: tmp) {
+            sum += iter;
+        }
+        result.push_back(sum / tmp.size());
+    }
+    return result;
+}
