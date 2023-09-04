4efac9cb9ee8b4ee474fd57e3f3d1a70aa7abd64
Ogrebatel <ogrebatelb@gmail.com>
2023-09-04 16:10:28 +0300

    in process

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,21 @@
 #include "Solution.h"
 
+int Solution::kthSmallest(TreeNode *root, int k) {
+    return inorder(root, &k).second;
+}
+
+std::pair<bool, int> Solution::inorder(TreeNode *node, int *k) {
+    if (!node) {
+        return {false, 0};
+    }
+
+    auto result = inorder(node->left, k);
+    if (result.first) {
+        return result;
+    }
+    --(*k);
+    if (*k == 0) {
+        return {true, node->val};
+    }
+    return inorder(node->right, k);
+}
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -3,8 +3,23 @@
 
 TEST(Test, Example1) {
     Solution sol;
+    TreeNode node1(4);
+    TreeNode node2(2);
+    TreeNode node3(6);
+    TreeNode node4(1);
+    TreeNode node5(3);
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+    node1.left = &node2;
+    node1.right = &node3;
+
+    node2.left = &node4;
+    node2.right = &node5;
+    EXPECT_EQ(1, sol.kthSmallest(&node1, 1));
+    EXPECT_EQ(2, sol.kthSmallest(&node1, 2));
+    EXPECT_EQ(3, sol.kthSmallest(&node1, 3));
+    EXPECT_EQ(4, sol.kthSmallest(&node1, 4));
+    EXPECT_EQ(6, sol.kthSmallest(&node1, 5));
+
 }
 
 int main() {
