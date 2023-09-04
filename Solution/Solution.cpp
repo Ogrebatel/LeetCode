e8bf1c1f78387adc2d8bab7287d535d71983ba64
Ogrebatel <ogrebatelb@gmail.com>
2023-09-04 14:56:54 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -10,15 +10,15 @@ int countHeight(TreeNode* node) {
     return std::max(countHeight(node->left), countHeight(node->right)) + 1;
 }
 
-void levelOrder(TreeNode* node, int i) {
+void levelOrder(TreeNode* node, int i, int position, std::vector<int> *vec) {
     if (!node) {
         return;
     }
     if (i == 0) {
-        std::cout << node->val << std::endl;
+        vec->push_back(node->val);
     } else {
-        levelOrder(node->left, i - 1);
-        levelOrder(node->right, i - 1);
+        levelOrder(node->left, i - 1, position - 1, vec);
+        levelOrder(node->right, i - 1, position + 1, vec);
     }
 }
 
@@ -26,9 +26,12 @@ std::vector<int> Solution::rightSideView(TreeNode *root) {
     if (!root) {
         return std::vector<int>();
     }
-
+    std::vector<int> result;
     int height = countHeight(root);
     for (int i = 0; i < height; ++i) {
-        levelOrder(root, i);
+        std::vector<int> tmp;
+        levelOrder(root, i, 0, &tmp);
+        result.push_back(tmp[tmp.size() - 1]);
     }
+    return result;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -3,8 +3,24 @@
 
 TEST(Test, Example1) {
     Solution sol;
+    TreeNode node1(1);
+    TreeNode node2(2);
+    TreeNode node3(3);
+    TreeNode node4(4);
+    TreeNode node5(5);
+    TreeNode node6(6);
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+    node1.right = &node2;
+    node1.right = &node3;
+
+    node2.right = &node5;
+    node3.right = &node4;
+
+    node5.right = &node6;
+    auto result = sol.rightSideView(&node1);
+    for (const auto &iter: result) {
+        std::cout << iter << " ";
+    }
 }
 
 int main() {
