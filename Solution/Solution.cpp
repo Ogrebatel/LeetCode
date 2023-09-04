3ffddc89dd8ee9c0700b36ee15c62c01ff9b4534
Ogrebatel <ogrebatelb@gmail.com>
2023-09-04 14:23:51 +0300

    in process

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,34 @@
 #include "Solution.h"
 
+int countHeight(TreeNode* node) {
+    if (!node) {
+        return 0;
+    }
+    if (!node->left && !node->right) {
+        return 1;
+    }
+    return std::max(countHeight(node->left), countHeight(node->right)) + 1;
+}
+
+void levelOrder(TreeNode* node, int i) {
+    if (!node) {
+        return;
+    }
+    if (i == 0) {
+        std::cout << node->val << std::endl;
+    } else {
+        levelOrder(node->left, i - 1);
+        levelOrder(node->right, i - 1);
+    }
+}
+
+std::vector<int> Solution::rightSideView(TreeNode *root) {
+    if (!root) {
+        return std::vector<int>();
+    }
+
+    int height = countHeight(root);
+    for (int i = 0; i < height; ++i) {
+        levelOrder(root, i);
+    }
+}
--- Solution/Solution.h
+++ Solution/Solution.h
@@ -1,10 +1,24 @@
 #ifndef LEETCODE_SOLUTION_H
 #define LEETCODE_SOLUTION_H
 
+#include <vector>
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
- public:
-    static int solution(int a, int b);
+public:
+    std::vector<int> rightSideView(TreeNode *root);
 };
 
 
