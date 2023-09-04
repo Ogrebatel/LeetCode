a5b6d64e2dadfd4bc303722b479650c3b8a9e642
Ogrebatel <ogrebatelb@gmail.com>
2023-09-04 13:23:31 +0300

    in process

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,25 @@
 #include "Solution.h"
 
+BSTIterator::BSTIterator(TreeNode *root) : iter(root) {
+    while (iter) {
+        _nodes.push(iter);
+        iter = iter->left;
+    }
+}
+
+int BSTIterator::next() {
+    while (iter != nullptr) {
+        _nodes.push(iter);
+        iter = iter->left;
+    }
+
+    iter = _nodes.top();
+    _nodes.pop();
+    int val = iter->val;
+    iter = iter->right;
+    return val;
+}
+
+bool BSTIterator::hasNext() {
+    return false;
+}
--- Solution/Solution.h
+++ Solution/Solution.h
@@ -1,10 +1,31 @@
 #ifndef LEETCODE_SOLUTION_H
 #define LEETCODE_SOLUTION_H
 
+#include <stack>
 
-class Solution {
- public:
-    static int solution(int a, int b);
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root);
+
+    int next();
+
+    bool hasNext();
+
+private:
+    std::stack<TreeNode*> _nodes;
+    TreeNode* iter;
 };
 
 
