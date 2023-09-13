587d257ab1e2def02d8c368b3c798dbb1840e41a
ogrebatel <ogrebatelb@gmail.com>
2023-09-13 16:31:26 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,5 +1,41 @@
 #include "Solution.h"
 
-int Solution::solution(int a, int b) {
-    return a + b;
+ListNode *Solution::partition(ListNode *head, int x) {
+    ListNode* prevNodes = nullptr;
+    ListNode* postNodes = nullptr;
+
+    ListNode* inputIter = head;
+    ListNode* prevNodesIter = nullptr;
+    ListNode* postNodesIter = nullptr;
+
+    while (inputIter) {
+        if (inputIter->val < x) {
+            if (!prevNodes) {
+                prevNodes = inputIter;
+                prevNodesIter = prevNodes;
+            } else {
+                prevNodesIter->next = inputIter;
+                prevNodesIter = prevNodesIter->next;
+            }
+        } else {
+            if (!postNodes) {
+                postNodes = inputIter;
+                postNodesIter = postNodes;
+            } else {
+                postNodesIter->next = inputIter;
+                postNodesIter = postNodesIter->next;
+            }
+        }
+        inputIter = inputIter->next;
+    }
+
+    if (postNodesIter) {
+        postNodesIter->next = nullptr;
+    }
+    if (prevNodesIter) {
+        prevNodesIter->next = postNodes;
+        return prevNodes;
+    }
+
+    return head;
 }
