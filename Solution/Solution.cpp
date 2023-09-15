6b14916eb07ac393a5e177174a623a2601620881
Ogrebatel <ogrebatelb@gmail.com>
2023-09-15 20:32:47 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,9 @@
 #include "Solution.h"
 
+int Solution::singleNumber(std::vector<int> &nums) {
+    int result = 0;
+    for (const auto &item: nums) {
+        result ^= item;
+    }
+    return result;
+}
