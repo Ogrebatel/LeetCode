34296e01ce731ec39b4d7dafacc3828251f19f21
ogrebatel <ogrebatelb@gmail.com>
2023-09-20 15:53:04 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,19 @@
 #include "Solution.h"
 
+int Solution::rob(std::vector<int> &nums) {
+    if (nums.size() == 1) {
+        return nums[0];
+    }
+    if (nums.size() == 2) {
+        return std::max(nums[0], nums[1]);
+    }
+
+    std::vector<int> maxRob(nums.size());
+    maxRob[0] = nums[0];
+    maxRob[1] = std::max(nums[0], nums[1]);
+
+    for (int i = 2; i < nums.size(); ++i) {
+        maxRob[i] = std::max(maxRob[i - 2] + nums[i], maxRob[i - 1]);
+    }
+    return maxRob[maxRob.size() - 1];
+}
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -4,7 +4,28 @@
 TEST(Test, Example1) {
     Solution sol;
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+    std::vector<int> nums = {1,2,3,1};
+    int expected = 4;
+
+    EXPECT_EQ(expected, sol.rob(nums));
+}
+
+TEST(Test, Example2) {
+    Solution sol;
+
+    std::vector<int> nums = {2,7,9,3,1};
+    int expected = 12;
+
+    EXPECT_EQ(expected, sol.rob(nums));
+}
+
+TEST(Test, Example3) {
+    Solution sol;
+
+    std::vector<int> nums = {2,1,1,2};
+    int expected = 4;
+
+    EXPECT_EQ(expected, sol.rob(nums));
 }
 
 int main() {
