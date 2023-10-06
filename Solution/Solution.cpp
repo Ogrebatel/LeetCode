0baaaa65bad5bd87fbde9a3702057f80bccc97f2
Ogrebatel <ogrebatelb@gmail.com>
2023-10-06 21:09:10 +0300

    done

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -1,2 +1,21 @@
 #include "Solution.h"
 
+int Solution::findMin(std::vector<int> &nums) {
+    int leftIdx = 0;
+    int rightIdx = nums.size() - 1;
+
+    while (true) {
+
+        int centerIdx = (rightIdx - leftIdx) / 2 + leftIdx;
+
+        if (nums[centerIdx] > nums[leftIdx] && nums[centerIdx] > nums[rightIdx]) {
+            leftIdx = centerIdx + 1;
+        } else if (nums[centerIdx] < nums[leftIdx] && nums[centerIdx] < nums[rightIdx]) {
+            rightIdx = centerIdx;
+        } else if (nums[leftIdx] <= nums[rightIdx]) {
+            return nums[leftIdx];
+        } else {
+            return nums[rightIdx];
+        }
+    }
+}
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -3,8 +3,42 @@
 
 TEST(Test, Example1) {
     Solution sol;
+    std::vector<int> nums = {3,4,5,1,2};
+    int expected = 1;
 
-    EXPECT_EQ(4, sol.solution(2, 2));
+    EXPECT_EQ(expected, sol.findMin(nums));
+}
+
+TEST(Test, Example2) {
+    Solution sol;
+    std::vector<int> nums = {4,5,6,7,0,1,2};
+    int expected = 0;
+
+    EXPECT_EQ(expected, sol.findMin(nums));
+}
+
+TEST(Test, Example3) {
+    Solution sol;
+    std::vector<int> nums = {11,13,15,17};
+    int expected = 11;
+
+    EXPECT_EQ(expected, sol.findMin(nums));
+}
+
+TEST(Test, Example4) {
+    Solution sol;
+    std::vector<int> nums = {11,13};
+    int expected = 11;
+
+    EXPECT_EQ(expected, sol.findMin(nums));
+}
+
+TEST(Test, Example5) {
+    Solution sol;
+    std::vector<int> nums = {13,11};
+    int expected = 11;
+
+    EXPECT_EQ(expected, sol.findMin(nums));
 }
 
 int main() {
